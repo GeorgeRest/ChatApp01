38ff5cdf6ee22d8df0ff66bde0c10f72e886ea0b
George <1561903398@qq.com>
2023-04-21 11:54:58 +0800

    george-3

--- app/src/main/java/com/george/chatapp/MainActivity.java
+++ app/src/main/java/com/george/chatapp/MainActivity.java
@@ -33,8 +33,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         userState = getSharedPreferences("userState", MODE_PRIVATE);
-
-
         //跳转注册
         register.setOnClickListener(new View.OnClickListener() {
             @Override
